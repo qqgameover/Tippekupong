c5d79829fb1f8321fd09e5f800e705a09ad86b7e
qqgameover <kasper35@live.no>
2021-10-05 23:01:50 +0200

    yeet

---
+++ Tippekupong/Match.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tippekupong
+{ 
+    class Match
+    {
+
+        public int HomeGoals;
+        public int AwayGoals;
+        public bool MatchIsRunning;
+        public string Bet;
+
+        public Match()
+        {
+            HomeGoals = 0;
+            AwayGoals = 0;
+            MatchIsRunning = false;
+        }
+
+        public void StartMatch()
+        {
+            Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nHva har du tippet for denne kampen? ");
+            MatchIsRunning = true;
+            Bet = Console.ReadLine();
+            while (MatchIsRunning)
+            {
+                Console.Write("Kommandoer: \r\n - H = scoring hjemmelag\r\n - B = scoring bortelag\r\n - X = kampen er ferdig\r\nAngi kommando: ");
+                var command = Console.ReadLine();
+                MatchIsRunning = command != "X";
+                if (command == "H") HomeGoals++;
+                else if (command == "B") AwayGoals++;
+                Console.WriteLine($"Stillingen er {HomeGoals}-{AwayGoals}.");
+            }
+        }
+    }
+}
--- Tippekupong/Program.cs
+++ Tippekupong/Program.cs
@@ -6,7 +6,12 @@ namespace Tippekupong
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var match = new Match();
+            match.StartMatch();
+            var result = match.HomeGoals == match.AwayGoals ? "U" : match.HomeGoals > match.AwayGoals ? "H" : "B";
+            var isBetCorrect = match.Bet.Contains(result);
+            var isBetCorrectText = isBetCorrect ? "riktig" : "feil";
+            Console.WriteLine($"Du tippet {isBetCorrectText}");
         }
     }
 }
