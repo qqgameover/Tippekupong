b471199217ac64b4c5c613cbfaf15323eb858f2b
qqgameover <kasper35@live.no>
2021-10-05 22:39:59 +0200

    Initial commit

---
+++ Tippekupong/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tippekupong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
