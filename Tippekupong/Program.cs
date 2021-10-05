61e902ced0109912fa1bb7d2ddd1efff5eae5a2c
qqgameover <kasper35@live.no>
2021-10-06 00:08:48 +0200

    Update Program.cs

--- Tippekupong/Program.cs
+++ Tippekupong/Program.cs
@@ -9,11 +9,11 @@ namespace Tippekupong
             var match = new Match();
             match.StartMatch();
             Console.Write("Gyldig tips: \r\n - H, U, B\r\n - halvgardering: HU, HB, UB\r\n - helgardering: HUB\r\nHva har du tippet for denne kampen? ");
-            match.Bet = Console.ReadLine().ToUpper();
+            match.Bet = Console.ReadLine()?.ToUpper();
             while (match.MatchIsRunning)
             {
                 Console.Write("Kommandoer: \r\n - H = scoring hjemmelag\r\n - B = scoring bortelag\r\n - X = kampen er ferdig\r\nAngi kommando: ");
-                var command = Console.ReadLine().ToUpper();
+                var command = Console.ReadLine()?.ToUpper();
                 match.AddGoal(command);
             }
             match.IsBetCorrectText();
